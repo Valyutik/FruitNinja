b19569d594183681e6411e491d1c73e0a51e25c3
Valyutik <lind-2004@mail.ru>
2023-11-26 13:43:47 +0300

    Добавлен счетчик очков

---
+++ Assets/_Project/Scripts/Score.cs
@@ -0,0 +1,32 @@
+using TMPro;
+
+namespace _Project.Scripts
+{
+    public sealed class Score
+    {
+        private readonly TMP_Text _scoreText;
+        private int _score;
+
+        public Score(TMP_Text scoreText)
+        {
+            _scoreText = scoreText;
+            SetScore(0);
+        }
+        
+        public void AddScore(int value)
+        {
+            SetScore(_score + value);
+        }
+        
+        private void SetScore(int value)
+        {
+            _score = value;
+            SetScoreText(value);
+        }
+        
+        private void SetScoreText(int value)
+        {
+            _scoreText.text = "Очки: " + value;
+        }
+    }
+}
